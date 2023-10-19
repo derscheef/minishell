51eeba146633ae8ca0ec6b0db5b50720d08bd835
derscheef <yannic.scheef@gmail.com>
2023-10-19 18:24:52 +0200

    Signal WIP

--- src/signals/main.c
+++ src/signals/main.c
@@ -6,24 +6,24 @@
 /*   By: yscheef <yscheef@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 13:43:59 by ndivjak           #+#    #+#             */
-/*   Updated: 2023/10/19 14:07:16 by yscheef          ###   ########.fr       */
+/*   Updated: 2023/10/19 14:13:17 by yscheef          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "signal.h"
 
-void	handle_signals(void)
-{
-	signal(SIGINT, sigint_handler);
-	signal(SIGTERM, sigterm_handler);
-	signall(SIGKILL, sigkill_handler);
-	signal(SIGSEV, sigsev_handler);
-}
-
+// ctrl-c
 void	sigint_handler(int signum)
 {
 	(void)signum;
 	printf("SIGINT\n");
+}
+
+// ctrl-D
+void	sigquit_handler(int signum)
+{
+	(void)signum;
+	printf("SIGQUIT\n");
 }
 
 void	sigterm_handler(int signum)
@@ -38,8 +38,10 @@ void	sigkill_handler(int signum)
 	printf("SIGKILL\n");
 }
 
-void	sigsev_handler(int signum)
+void	handle_signals(void)
 {
-	(void)signum;
-	printf("SIGSEV\n");
+	signal(SIGINT, sigint_handler);
+	signal(SIGTERM, sigterm_handler);
+	signall(SIGKILL, sigkill_handler);
+	signal(SIGQUIT, sigquit_handler);
 }
