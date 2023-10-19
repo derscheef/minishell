26b67e8d3b660a46e8ec04def0f1008ebe7015a5
derscheef <yannic.scheef@gmail.com>
2023-10-19 13:41:58 +0200

    test

--- src/signals/main.c
+++ src/signals/main.c
@@ -1,3 +1,33 @@
+#include "signal.h"
+
 void	handle_signals(void)
 {
+	signal(SIGINT, sigint_handler);
+	signal(SIGTERM, sigterm_handler);
+	signall(SIGKILL, sigkill_handler);
+	signal(SIGSEV, sigsev_handler);
+}
+
+void	sigint_handler(int signum)
+{
+	(void)signum;
+	printf("SIGINT\n");
+}
+
+void	sigterm_handler(int signum)
+{
+	(void)signum;
+	printf("SIGTERM\n");
+}
+
+void	sigkill_handler(int signum)
+{
+	(void)signum;
+	printf("SIGKILL\n");
+}
+
+void	sigsev_handler(int signum)
+{
+	(void)signum;
+	printf("SIGSEV\n");
 }
