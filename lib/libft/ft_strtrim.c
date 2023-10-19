edd32c4a5bd3cfca36d35541ffffc267ca2ff4fb
Nik Divjak <divjak.nik@gmail.com>
2023-10-19 17:40:50 +0200

    nvm revert strtrim update

--- lib/libft/ft_strtrim.c
+++ lib/libft/ft_strtrim.c
@@ -6,45 +6,97 @@
 /*   By: ndivjak <ndivjak@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 01:15:32 by ndivjak           #+#    #+#             */
-/*   Updated: 2023/10/19 17:33:15 by ndivjak          ###   ########.fr       */
+/*   Updated: 2023/10/19 17:40:38 by ndivjak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_char_in_set(char c, char const *set)
+char	*ft_shitstr(const char *s, unsigned int start, size_t len)
 {
+	char	*sub;
+
+	if (!s)
+		return (0);
+	if ((unsigned int)ft_strlen(s) < start)
+		len = 0;
+	if ((unsigned int)ft_strlen(s + start) < len)
+		len = ft_strlen(s + start);
+	sub = malloc(sizeof(char) * (len + 1));
+	if (!sub)
+		return (0);
+	ft_strlcpy(sub, (char *)s + start, len + 1);
+	return (sub);
+}
+
+char	*ft_start(char *s_pos, char *set)
+{
+	char	*chk;
 	size_t	i;
 
 	i = 0;
-	while (set[i])
+	while (*s_pos && *set)
 	{
-		if (set[i] == c)
-			return (1);
-		i++;
+		chk = ft_strchr(s_pos, set[i]);
+		if (i == ft_strlen(set) - 1)
+		{
+			if (chk != s_pos)
+				break ;
+			i = 0;
+		}
+		if (chk == s_pos)
+		{
+			s_pos++;
+			i = 0;
+		}
+		else
+			i++;
 	}
-	return (0);
+	return (s_pos);
+}
+
+int	ft_end(char *s1, char *set)
+{
+	size_t	i;
+	size_t	end;
+	char	*chk;
+
+	i = 0;
+	end = 0;
+	while (*s1 && *set)
+	{
+		chk = ft_strchr(s1, set[i]);
+		if (i == ft_strlen(set) - 1)
+		{
+			if (chk != s1)
+				break ;
+			i = 0;
+		}
+		if (chk == s1)
+		{
+			s1--;
+			end++;
+			i = 0;
+		}
+		else
+			i++;
+	}
+	return (end);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*str;
-	size_t	i;
-	size_t	start;
 	size_t	end;
+	char	*s_pos;
 
-	start = 0;
-	while (s1[start] && ft_char_in_set(s1[start], set))
-		start++;
-	end = ft_strlen(s1);
-	while (end > start && ft_char_in_set(s1[end - 1], set))
-		end--;
-	str = (char *)malloc(sizeof(*s1) * (end - start + 1));
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (start < end)
-		str[i++] = s1[start++];
-	str[i] = 0;
-	return (str);
+	if (s1 && set)
+	{
+		s_pos = (char *)s1;
+		s_pos = ft_start(s_pos, (char *)set);
+		s1 += ft_strlen(s1) - 1;
+		end = ft_end((char *)s1, (char *)set);
+		return ((char *)ft_shitstr(s_pos, 0, ft_strlen(s_pos) - end));
+	}
+	else
+		return (0);
 }
