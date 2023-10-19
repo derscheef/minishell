48cf34066c31fe65d78937115238e598e00da70f
Nik Divjak <divjak.nik@gmail.com>
2023-10-19 14:10:23 +0200

    fixed signals

--- src/signals/main.c
+++ src/signals/main.c
@@ -3,22 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yscheef <yscheef@student.42vienna.com>     +#+  +:+       +#+        */
+/*   By: ndivjak <ndivjak@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 13:43:59 by ndivjak           #+#    #+#             */
-/*   Updated: 2023/10/19 14:07:16 by yscheef          ###   ########.fr       */
+/*   Updated: 2023/10/19 14:09:55 by ndivjak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "signal.h"
-
-void	handle_signals(void)
-{
-	signal(SIGINT, sigint_handler);
-	signal(SIGTERM, sigterm_handler);
-	signall(SIGKILL, sigkill_handler);
-	signal(SIGSEV, sigsev_handler);
-}
 
 void	sigint_handler(int signum)
 {
@@ -38,8 +30,9 @@ void	sigkill_handler(int signum)
 	printf("SIGKILL\n");
 }
 
-void	sigsev_handler(int signum)
+void	handle_signals(void)
 {
-	(void)signum;
-	printf("SIGSEV\n");
+	signal(SIGINT, sigint_handler);
+	signal(SIGTERM, sigterm_handler);
+	signall(SIGKILL, sigkill_handler);
 }
