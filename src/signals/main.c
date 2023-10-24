ff2a1b080e7b5cbbca2358b21246179eb8ff715c
derscheef <yannic.scheef@gmail.com>
2023-10-24 13:21:15 +0200

    finished Signals

--- src/signals/main.c
+++ src/signals/main.c
@@ -6,7 +6,7 @@
 /*   By: yscheef <yscheef@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 13:43:59 by ndivjak           #+#    #+#             */
-/*   Updated: 2023/10/24 11:30:07 by yscheef          ###   ########.fr       */
+/*   Updated: 2023/10/24 12:48:39 by yscheef          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,40 +18,52 @@ void	ft_putstr(char *str)
 		write(1, str++, 1);
 }
 
-// ctrl-C
-// rl_replace_line("", 0);
-void	sigint_handler(int signum)
+void	configure_terminal(void)
 {
-	if (signum == SIGUSR1)
-		exit(EXIT_FAILURE);
-	if (signum == SIGINT)
-	{
-		ft_putstr("\n");
-		rl_on_new_line();
-		rl_redisplay();
-	}
-	if (signum == SIGTERM)
-	{
-		exit(1);
-	}
+	struct termios	term;
+
+	// Get the current terminal settings
+	tcgetattr(STDIN_FILENO, &term);
+	// Modify the settings
+	term.c_lflag &= ~(ECHOCTL);
+	// Set the new terminal settings
+	tcsetattr(STDIN_FILENO, TCSANOW, &term);
+}
+
+// ctrl-C
+void	handle_sigint(int sig)
+{
+	(void)sig;
+	write(STDOUT_FILENO, "\n", 1);
+	rl_on_new_line();
+	rl_redisplay();
+	ft_putstr("minishell> ");
+	configure_terminal();
 }
 
 // ctrl-backslash
-void	sigquit_handler(int signum)
+void	handle_sigquit(int sig)
 {
-	(void)signum;
+	(void)sig;
 	exit(1);
+}
+
+void	handle_eof(void)
+{
+	// Exit the shell
+	exit(0);
 }
 
 void	handle_signals(void)
 {
-	struct termios	term;
-	struct termios	oldterm;
-
-	tcgetattr(STDIN_FILENO, &oldterm);
-	term = oldterm;
-	term.c_lflag &= ~(ICANON | ECHO);
-	tcsetattr(STDIN_FILENO, TCSANOW, &term);
-	signal(SIGINT, sigint_handler);
-	signal(SIGQUIT, sigquit_handler);
+	struct sigaction sa_int, sa_quit;
+	configure_terminal();
+	sa_int.sa_handler = handle_sigint;
+	sigemptyset(&sa_int.sa_mask);
+	sa_int.sa_flags = SA_RESTART;
+	sigaction(SIGINT, &sa_int, NULL);
+	sa_quit.sa_handler = handle_sigquit;
+	sigemptyset(&sa_quit.sa_mask);
+	sa_quit.sa_flags = SA_RESTART;
+	sigaction(SIGQUIT, &sa_quit, NULL);
 }
