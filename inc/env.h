cb72f051ee7f6539c97df1aa1362daf115069666
derscheef <yannic.scheef@gmail.com>
2023-10-25 16:08:58 +0200

    fixed stupid norm

--- inc/env.h
+++ inc/env.h
@@ -6,7 +6,7 @@
 /*   By: yscheef <yscheef@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 14:14:59 by yscheef           #+#    #+#             */
-/*   Updated: 2023/10/25 14:21:21 by yscheef          ###   ########.fr       */
+/*   Updated: 2023/10/25 16:08:18 by yscheef          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ typedef struct s_env_node
 }						t_env_node;
 
 t_env_node				*init_env_list(char **env);
-void					print_env_list(t_env_node *env_list);
+t_env_node				*new_env_node(char *str);
+void	env_lstadd_back(t_env_node **lst, t_env_node *new)
 
 #endif
--- src/env/init.c
+++ src/env/init.c
@@ -6,42 +6,59 @@
 /*   By: yscheef <yscheef@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 14:14:46 by yscheef           #+#    #+#             */
-/*   Updated: 2023/10/25 14:16:51 by yscheef          ###   ########.fr       */
+/*   Updated: 2023/10/25 16:07:22 by yscheef          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	env_lstadd_back(t_env_node **lst, t_env_node *new)
+{
+	t_env_node	*last;
+
+	if (!lst || !new)
+		return ;
+	if (!*lst)
+		*lst = new;
+	else
+	{
+		last = *lst;
+		while (last->next)
+			last = last->next;
+		last->next = new;
+	}
+}
+
+t_env_node	*new_env_node(char *str)
+{
+	t_env_node	*node;
+	char		*delimiter;
+
+	node = malloc(sizeof(t_env_node));
+	if (!node)
+		return (NULL);
+	delimiter = ft_strchr(str, '=');
+	node->key = ft_substr(str, 0, delimiter - str);
+	node->value = ft_strdup(delimiter + 1);
+	node->next = NULL;
+	return (node);
+}
+
 t_env_node	*init_env_list(char **env)
 {
-	t_env_node *head;
-	t_env_node *temp;
-	char *key;
-	char *value;
-	int i;
-	t_env_node *node;
+	t_env_node	*head;
+	t_env_node	*temp;
+	int			i;
 
 	head = NULL;
-	temp = NULL;
 	i = 0;
-	while (env[i] != NULL)
+	while (env[i])
 	{
-		key = strtok(env[i], "=");
-		value = strtok(NULL, "=");
-		node = malloc(sizeof(t_env_node));
-		node->key = strdup(key);
-		node->value = strdup(value);
-		node->next = NULL;
-		if (head == NULL)
-		{
-			head = node;
-			temp = node;
-		}
+		temp = new_env_node(env[i]);
+		if (!head)
+			head = temp;
 		else
-		{
-			temp->next = node;
-			temp = temp->next;
-		}
+			env_lstadd_back(&head, temp);
 		i++;
 	}
 	return (head);
