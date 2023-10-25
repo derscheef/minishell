2739e350fcd548c4ceb232e59b0525ee9ce40043
derscheef <yannic.scheef@gmail.com>
2023-10-25 16:09:41 +0200

    fuckkkkk norm

--- inc/env.h
+++ inc/env.h
@@ -6,7 +6,7 @@
 /*   By: yscheef <yscheef@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 14:14:59 by yscheef           #+#    #+#             */
-/*   Updated: 2023/10/25 16:08:18 by yscheef          ###   ########.fr       */
+/*   Updated: 2023/10/25 16:09:30 by yscheef          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ typedef struct s_env_node
 
 t_env_node				*init_env_list(char **env);
 t_env_node				*new_env_node(char *str);
-void	env_lstadd_back(t_env_node **lst, t_env_node *new)
+void					env_lstadd_back(t_env_node **lst, t_env_node *new);
 
 #endif
