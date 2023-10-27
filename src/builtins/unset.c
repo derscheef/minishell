90504efd437b74695a2ec40ab4fa710f7a9cab6a
derscheef <yannic.scheef@gmail.com>
2023-10-27 10:45:35 +0200

    f norm

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: yscheef <yscheef@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 16:40:26 by yscheef           #+#    #+#             */
-/*   Updated: 2023/10/26 16:46:26 by yscheef          ###   ########.fr       */
+/*   Updated: 2023/10/27 10:45:12 by yscheef          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,7 @@
 
 char	*parse_key(char *input)
 {
-	// Assuming the input format is "unset KEY", we'll skip the first 6 chars
 	return (ft_strtrim(input + 6, " \t\n\r\v\f"));
-	// remove any surrounding whitespace
 }
 
 void	remove_env_node(t_env_node **head, char *key)
