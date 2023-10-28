9bc62d5d7b723a238de60b70ab263c43fa74bd74
Nik Divjak <nik.divjak@scheef.tech>
2023-10-28 19:34:48 +0200

    WIP Executor

--- src/executor/pipe.c
+++ src/executor/pipe.c
@@ -6,7 +6,7 @@
 /*   By: ndivjak <ndivjak@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 17:02:28 by ndivjak           #+#    #+#             */
-/*   Updated: 2023/10/28 18:47:24 by ndivjak          ###   ########.fr       */
+/*   Updated: 2023/10/28 19:33:59 by ndivjak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,31 @@
 // fd[1] = write
 void	execute_pipe(t_executor *p)
 {
-	int	fd[2];
+	int		fd[2];
+	t_node	*node;
+	int		read_pipe;
+	int		write_pipe;
 
 	pipe(fd);
-	execute_command((t_cmd){p->node->left, p->env, false, true, 0, fd[1], NULL,
+	read_pipe = fd[0];
+	write_pipe = fd[1];
+	execute_command((t_cmd){p->node->right, p->env, false, true, 0, write_pipe,
+		NULL, NULL});
+	node = p->node->left;
+	while (node && node->type == NODE_PIPE)
+	{
+		close(write_pipe);
+		pipe(fd);
+		write_pipe = fd[1];
+		execute_command((t_cmd){node->right, p->env, true, true, read_pipe,
+			write_pipe, NULL, NULL});
+		close(read_pipe);
+		read_pipe = fd[0];
+		node = node->left;
+	}
+	read_pipe = fd[0];
+	close(write_pipe);
+	execute_command((t_cmd){node, p->env, true, false, read_pipe, 0, NULL,
 		NULL});
+	close(read_pipe);
 }
