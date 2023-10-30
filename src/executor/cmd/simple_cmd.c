f181f6362c3cfa8d850b06bbccabed86859b1272
Nik Divjak <nik.divjak@scheef.tech>
2023-10-30 16:03:35 +0100

    Fixed function length

--- src/executor/cmd/simple_cmd.c
+++ src/executor/cmd/simple_cmd.c
@@ -6,7 +6,7 @@
 /*   By: ndivjak <ndivjak@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 17:06:16 by ndivjak           #+#    #+#             */
-/*   Updated: 2023/10/30 10:58:46 by ndivjak          ###   ########.fr       */
+/*   Updated: 2023/10/30 16:03:02 by ndivjak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,26 @@
 static bool	init_arguments(t_cmd *p, t_internal_cmd *cmd)
 {
 	t_node	*node;
-	size_t	ac;
 
 	if (!p->node || p->node->type != NODE_CMDPATH)
 		return (true);
 	node = p->node;
-	ac = 0;
+	cmd->ac = 0;
 	while (node && (node->type == NODE_ARGUMENT || node->type == NODE_CMDPATH))
 	{
-		ac++;
+		cmd->ac++;
 		node = node->left;
 	}
-	cmd->ac = ac;
-	cmd->av = ft_calloc(ac + 1, sizeof(char *));
+	cmd->av = ft_calloc(cmd->ac + 1, sizeof(char *));
 	if (!cmd->av)
 		return (true);
-	cmd->av[ac] = NULL;
+	cmd->av[cmd->ac] = NULL;
 	node = p->node;
-	ac = 0;
+	cmd->ac = 0;
 	while (node && (node->type == NODE_ARGUMENT || node->type == NODE_CMDPATH))
 	{
-		cmd->av[ac] = ft_strdup(node->data);
-		ac++;
+		cmd->av[cmd->ac] = ft_strdup(node->data);
+		cmd->ac++;
 		node = node->left;
 	}
 	return (false);
