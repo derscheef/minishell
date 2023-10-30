4c275391675a8b959e05c8e7d8e76b6ccf3d77ae
derscheef <yannic.scheef@gmail.com>
2023-10-30 19:00:41 +0100

    updated exit routine

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,15 +6,28 @@
 /*   By: yscheef <yscheef@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 17:03:46 by yscheef           #+#    #+#             */
-/*   Updated: 2023/10/27 11:00:53 by yscheef          ###   ########.fr       */
+/*   Updated: 2023/10/30 19:00:41 by yscheef          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 // TODO: Change to rl_clear_history from clear_history
-int	exit_routine(void)
+void	exit_routine(char *exit_code_str)
 {
+	int	exit_code;
+
+	while (*exit_code_str)
+	{
+		if (!ft_isdigit(*exit_code_str) && !(*exit_code_str == '+'
+				|| *exit_code_str == '-'))
+		{
+			exit_code = 255;
+			perror("numeric argument required");
+			break ;
+		}
+		exit_code_str++;
+	}
 	clear_history();
-	return (exit(1), 0);
+	return (exit(exit_code));
 }
--- src/routine/main.c
+++ src/routine/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ndivjak <ndivjak@student.42vienna.com>     +#+  +:+       +#+        */
+/*   By: yscheef <yscheef@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 13:43:50 by ndivjak           #+#    #+#             */
-/*   Updated: 2023/10/30 13:05:51 by ndivjak          ###   ########.fr       */
+/*   Updated: 2023/10/30 18:49:24 by yscheef          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ void	routine(t_main *main)
 	{
 		main->input = readline("minishell> ");
 		if (!main->input)
-			exit_routine();
+			exit_routine(main->input);
 		add_history(main->input);
 		lexer(main->input, ft_strlen(main->input), &main->lexer,
 			main->env_list);
