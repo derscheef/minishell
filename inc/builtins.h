be126dff566c8277c5bf56bb464d4886fa8e384d
derscheef <yannic.scheef@gmail.com>
2023-10-30 15:31:52 +0100

    added filedescriptor

--- inc/builtins.h
+++ inc/builtins.h
@@ -6,7 +6,7 @@
 /*   By: yscheef <yscheef@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 16:22:03 by yscheef           #+#    #+#             */
-/*   Updated: 2023/10/30 15:14:56 by yscheef          ###   ########.fr       */
+/*   Updated: 2023/10/30 15:30:35 by yscheef          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@
 # include <term.h>
 
 int	clear_term(void);
-int	print_env(t_internal_cmd *main);
+int	print_env(t_internal_cmd *main, int fd);
 int	print_pwd(int fd);
-int	execute_echo(char *input);
+int	execute_echo(char *input, int fd);
 int	exec_export(char *input, t_internal_cmd *main);
 int	exec_unset(char *input, t_internal_cmd *main);
 int	exec_cd(char *input);
--- src/executor/cmd/exec_builtin.c
+++ src/executor/cmd/exec_builtin.c
@@ -6,7 +6,7 @@
 /*   By: yscheef <yscheef@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 17:46:20 by ndivjak           #+#    #+#             */
-/*   Updated: 2023/10/30 15:14:23 by yscheef          ###   ########.fr       */
+/*   Updated: 2023/10/30 15:29:29 by yscheef          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,9 +47,9 @@ bool	execute_builtin(t_internal_cmd *p)
 	else if (ft_strncmp(input, "exit", 4) == 0)
 		*p->exit_code = exit_routine();
 	else if (ft_strncmp(input, "echo", 4) == 0)
-		*p->exit_code = execute_echo(input);
+		*p->exit_code = execute_echo(input, fd);
 	else if (ft_strncmp(input, "pwd", 3) == 0)
-		*p->exit_code = print_pwd(p->fd_write);
+		*p->exit_code = print_pwd(fd);
 	else if (ft_strncmp(input, "cd", 2) == 0)
 		*p->exit_code = exec_cd(input);
 	else if (ft_strncmp(input, "export", 6) == 0)
@@ -57,7 +57,7 @@ bool	execute_builtin(t_internal_cmd *p)
 	else if (ft_strncmp(input, "unset", 5) == 0)
 		*p->exit_code = exec_unset(input, p);
 	else if (ft_strncmp(input, "env", 3) == 0)
-		*p->exit_code = print_env(p);
+		*p->exit_code = print_env(p, fd);
 	else if (ft_strncmp(input, "", 1) == 0)
 		;
 	else
