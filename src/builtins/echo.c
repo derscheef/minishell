9ae053315bc55bb3c4e262d0fb8943a9006323e7
derscheef <yannic.scheef@gmail.com>
2023-10-31 17:24:17 +0100

    added -n to echo

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: yscheef <yscheef@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 21:48:26 by yscheef           #+#    #+#             */
-/*   Updated: 2023/10/30 19:59:30 by yscheef          ###   ########.fr       */
+/*   Updated: 2023/10/31 17:23:33 by yscheef          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,19 @@
 int	execute_echo(t_internal_cmd *p, int fd)
 {
 	size_t	i;
+	int		nl;
 
+	nl = 1;
 	if (!p || fd < 0)
 	{
 		return (-1);
 	}
 	i = 1;
+	if (i < p->ac && strcmp(p->av[i], "-n") == 0)
+	{
+		nl = 0;
+		i++;
+	}
 	while (i < p->ac)
 	{
 		write(fd, p->av[i], strlen(p->av[i]));
@@ -28,6 +35,7 @@ int	execute_echo(t_internal_cmd *p, int fd)
 			write(fd, " ", 1);
 		i++;
 	}
-	write(fd, "\n", 1);
+	if (nl)
+		write(fd, "\n", 1);
 	return (0);
 }
