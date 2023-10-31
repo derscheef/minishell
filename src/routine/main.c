7d3da6fbbc82291a860e732318541fc3bdfbe5d9
Nik Divjak <nik.divjak@scheef.tech>
2023-10-31 11:14:24 +0100

    Fixed segv

--- src/routine/main.c
+++ src/routine/main.c
@@ -6,7 +6,7 @@
 /*   By: ndivjak <ndivjak@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 13:43:50 by ndivjak           #+#    #+#             */
-/*   Updated: 2023/10/30 19:06:05 by ndivjak          ###   ########.fr       */
+/*   Updated: 2023/10/31 11:14:14 by ndivjak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ static void	reset_routine(t_main *main)
 {
 	if (main->lexer.ntoks > 0)
 	{
+		main->lexer.ntoks = 0;
 		destroy_tokens(main->lexer.tokens);
 		destroy_ast(main->ast);
 	}
