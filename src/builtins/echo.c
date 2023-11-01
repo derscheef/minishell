c220c8f0ed78f5cf03a3488b86e5e0b997a70409
derscheef <yannic.scheef@gmail.com>
2023-11-01 16:26:33 +0100

    removed unnecessary

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: yscheef <yscheef@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 21:48:26 by yscheef           #+#    #+#             */
-/*   Updated: 2023/11/01 15:30:21 by yscheef          ###   ########.fr       */
+/*   Updated: 2023/11/01 15:45:40 by yscheef          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,11 @@ int	execute_echo(t_internal_cmd *p, int fd)
 	bool	is_nl;
 
 	is_nl = true;
-	nl = 1;
 	if (!p || fd < 0)
-	{
 		return (-1);
-	}
 	i = 1;
 	if (i < p->ac && strcmp(p->av[i], "-n") == 0)
-	{
-		nl = 0;
 		i++;
-	}
 	while (i < p->ac)
 	{
 		if (i == 1 && ft_strcmp(p->av[i], "-n") == 0)
